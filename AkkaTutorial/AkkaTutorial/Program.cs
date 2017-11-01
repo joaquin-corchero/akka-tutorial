12515fb5b6e37113e9f6f40488d57837e434999f
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-11-01 09:01:44 +0000

    Added the first actors

---
+++ AkkaTutorial/AkkaTutorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Akka.Actor;
+using System.Threading;
+
+namespace AkkaTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            StartSystem();
+            Console.ReadLine();
+        }
+
+        public static void StartSystem()
+        {
+            var system = ActorSystem.Create("MainActorSystem");
+            var firstRef = system.ActorOf(Props.Create<PrintMyActorRefActor>(), "first-actor");
+            Console.WriteLine($"First: {firstRef}");
+            firstRef.Tell("printit", ActorRefs.NoSender);
+        }
+    }
+
+}
