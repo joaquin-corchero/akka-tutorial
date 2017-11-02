8e257f4dea9c1200011ec5d2d7098ffbc48a3efa
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-11-02 09:37:40 +0000

    Started with device ator

---
+++ AkkaTutorial/AkkaTutorial/IOT/Actors/Device.cs
@@ -0,0 +1,37 @@
+﻿using Akka.Actor;
+using Akka.Event;
+using AkkaTutorial.IOT.Messages;
+
+namespace AkkaTutorial.IOT.Actors
+{
+    public class Device : UntypedActor
+    {
+        private readonly double? _lastTemperatureReading = null;
+        protected string GroupId { get; }
+        protected string DeviceId { get; }
+        protected ILoggingAdapter Log { get; } = Context.GetLogger();
+
+        public Device(string groupId, string deviceId)
+        {
+            GroupId = groupId;
+            DeviceId = deviceId;
+        }
+
+        protected override void PreStart() => Log.Info($"Device actor {GroupId}-{DeviceId} started");
+
+        protected override void PostStop() => Log.Info($"Device actor {GroupId}-{DeviceId} stopped");
+
+
+        protected override void OnReceive(object message)
+        {
+            switch (message)
+            {
+                case ReadTemperature read:
+                    Sender.Tell(new RespondTemperature(read.RequestId, _lastTemperatureReading));
+                    break;
+            }
+        }
+
+        public static Props Props(string groupId, string deviceId) => Akka.Actor.Props.Create(() => new Device(groupId, deviceId));
+    }
+}
---
+++ AkkaTutorial/AkkaTutorialTests/IOT/Actors/DeviceTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using Akka.TestKit.Xunit2;
+using AkkaTutorial.IOT.Actors;
+
+namespace AkkaTutorialTests.IOT.Actors
+{
+    public class When_working_with_the_device_actor : TestKit
+    {
+
+        [Fact]
+        public void Must_reply_with_empty_reading_if_no_temperature_is_known()
+        {
+            var probe = CreateTestProbe();
+            var deviceActor = Sys.ActorOf(Device.Props("group", "device"));
+        }
+    }
+}
